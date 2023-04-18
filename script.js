2ad852172f132fac5d63751113c03ab7c490915b
simonab101 <simo.buscicchio@gmail.com>
2023-04-18 11:49:20 +0200

    utilizzo data

---
+++ script.js
@@ -0,0 +1,45 @@
+let vue = new Vue({
+  el: "#myApp",
+  data: {
+  tasks: [
+  {
+    text: "Quarterly Newsletter",
+    done: true
+  },
+  {
+    text: "Rectruiting blog post",
+    done: true
+  },
+  {
+    text: "Mobile app launch",
+    done: true
+  },
+  {
+    text: "Interview with John H.",
+    done: false
+  },
+  {
+    text: "Summit update to mobile storefronts",
+    done: true
+  },
+  {
+    text: "Schedule meeting with Alex",
+    done: false
+  },
+  {
+    text: "Homepage refresh",
+    done: true
+  },
+  {
+    text: "Onboard new Sales team members",
+    done: true
+  },
+  {
+    text: "Review editorial calendar",
+    done: true
+  }
+  ],
+    inputTextValue: ""
+  },
+
+});
